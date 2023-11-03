0a0808f3cb55411ac34cbc6651356745c85e4c6a
kashyapAmansingh <coder85113@gmail.com>
2023-11-03 22:08:44 +0530

    first c

--- controllers/route.js
+++ controllers/route.js
@@ -30,7 +30,9 @@ app.use(express.json());
   }
 });
 
- 
+router.get("/", async (req, res) => {
+  res.send("you are on the home bro")
+})
 router.get("/booking", async (req, res) => {
   const myData = await bookMovieTicketSchema.find().sort({ _id: -1 }).limit(1);
  
